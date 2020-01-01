4fa25793180228910f06ea474abfad08b55c8c67
mehmetulus <43668582+mehmetulus@users.noreply.github.com>
2020-01-01 15:53:30 +0300

    Add files via upload

---
+++ seflink.php
@@ -0,0 +1,11 @@
+<?php
+/* Seflink Yapısı */
+
+		$find = array('Ç', 'Ş', 'Ğ', 'Ü', 'İ', 'Ö', 'ç', 'ş', 'ğ', 'ü', 'ö', 'ı', '+', '#','?','*','!','.','(',')');
+		$replace = array('c', 's', 'g', 'u', 'i', 'o', 'c', 's', 'g', 'u', 'o', 'i', 'plus', 'sharp','','','','','','');
+		$string = strtolower(str_replace($find, $replace, $val));
+		$string = preg_replace("@[^A-Za-z0-9\-_\.\+]@i", ' ', $string);
+		$string = trim(preg_replace('/\s+/', ' ', $string));
+		$string = str_replace(' ', '-', $string);
+		return $string;
+?>
